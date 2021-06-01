f06cbc86e45090a226605db852a7026d65d1688e
IvanGent <gentilleivan@gmail.com>
2021-06-01 16:05:48 -0400

    fixing line placement

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
-import './index.css';
 import App from './App';
 import configureStore from './store';
 import {restoreCSRF, fetch} from './store/csrf';
 import * as sessionActions from './store/session';
+import './index.css';
 
 const store = configureStore();
 
